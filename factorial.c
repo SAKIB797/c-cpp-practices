6a903dd73289d383da3afdc113b45170fa1bf3dc
Iftekhar Sakib <pro.iftekhar@gmail.com>
2023-10-27 00:35:31 +0600

    .

--- factorial.c
+++ factorial.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int i, fact=1,n;
+    int i, fact = 1, n;
+
+    // take the integer for its factorial
     printf("\nFactorial of - ");
-    scanf("%d",&n);
-    // statement for calculate the factorial value
-    for ( i = 1; i <= n; i++)
+    scanf("%d", &n);
+
+    // statement for calculating the factorial value
+    for (i = 1; i <= n; i++)
     {
-        fact*=i;
+        if (n < 0)
+        {
+            break;
+        }
+
+        fact *= i;
     }
-    printf("\nThe facetorial value of %d is %d",n,fact);
+
+    // print the factorial value
+    if (n < 0)
+    {
+        printf("\n>>> The factorial of %d is undefined\n", n);
+    }
+    else
+    {
+        printf("\n>>> The factorial of %d is %d\n", n, fact);
+    }
 
     return 0;
 }
