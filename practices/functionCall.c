1bc6f25e5ec42808cbfe4e04c7400a35b62619e1
Iftekhar Sakib <pro.iftekhar@gmail.com>
2023-11-04 19:38:48 +0600

    .

---
+++ practices/functionCall.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int sum()
+{
+    int x, y;
+    int sum = x + y;
+    return sum;
+////////main
+int main()
+{
+    int x, y;
+    printf("Value for first number: ");
+    scanf("%d", &x);
+    printf("Value for second number:");
+    scanf("%d", &y);
+    printf("%d", s);
+    return 0;
+}
